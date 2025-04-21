28b3f879e8b6166839cb7dc4b9370e1163a59b6f
Raghavi200434 <raghavimurugan3@gmail.com>
2025-04-21 21:55:48 +0530

    Added Sign Language Translator feature with styling and video playback

--- script.js
+++ script.js
@@ -19,3 +19,39 @@ function updateEmotionDisplay(emotion) {
     emotionImg.src = imagePath;
   }
   
+
+  const signVideos = {
+    "Hello": "videos/Hello.mp4",
+    "thank you": "videos/thank you.mp4",
+    "nice to meet you": "videos/nice to meet you.mp4",
+    "good_morning": "videos/good morning.mp4",
+    "see you later": "videos/see you later.mp4",
+    "happy": "videos/happy.mp4",
+    "how are you": "videos/how are you.mp4"
+    
+    
+  };
+
+  function playSignSequence(text) {
+    const words = text.toLowerCase().split("-");
+    const videoElement = document.getElementById("signVideo");
+  
+    let index = 0;
+  
+    function playNext() {
+      if (index < words.length) {
+        const word = words[index];
+        const src = signVideos[word] || `videos/${word}.mp4`; // fallback
+  
+        videoElement.src = src;
+        videoElement.play();
+  
+        index++;
+      }
+    }
+  
+    videoElement.onended = playNext; // when one ends, play the next
+    playNext(); // start first one
+  }
+  
+  
